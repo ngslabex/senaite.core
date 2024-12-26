25d6a202f070a9ccbb14dc44423387e1c795efaa
ngslabex <ersankose@gmail.com>
2024-12-26 03:36:06 +0300

    root

--- src/senaite/core/browser/samples/view.py
+++ src/senaite/core/browser/samples/view.py
@@ -85,7 +85,7 @@ class SamplesView(ListingView):
         self.contentFilter = {
             "sort_on": "created",
             "sort_order": "descending",
-            "isRootAncestor": False,  # only root ancestors
+            "isRootAncestor": True,  # only root ancestors
         }
 
         self.title = self.context.translate(_("Samples"))
